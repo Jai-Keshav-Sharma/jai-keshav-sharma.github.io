8e5de6094dd271360e5cd64e1605439c6d873409
Jai-Keshav-Sharma <ksharma9719@gmail.com>
2026-02-10 23:52:15 +0530

    Remove link preview functionality

--- src/components/MDXComponents.tsx
+++ src/components/MDXComponents.tsx
@@ -1,14 +1,8 @@
-import { LinkPreview } from "./LinkPreview";
 import { AnimatedPre } from "./AnimatedCodeBlock";
 import type { MDXComponents } from "mdx/types";
 
 export function getMDXComponents(): MDXComponents {
     return {
-        a: (props: React.AnchorHTMLAttributes<HTMLAnchorElement>) => {
-            const { href, children, ...rest } = props;
-            if (!href) return <a {...rest}>{children}</a>;
-            return <LinkPreview href={href}>{children}</LinkPreview>;
-        },
         pre: (props: React.HTMLAttributes<HTMLPreElement>) => {
             return <AnimatedPre {...props} />;
         },
